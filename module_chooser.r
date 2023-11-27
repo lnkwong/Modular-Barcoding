42853bea301545748476b57c66d25892d236ca09
lnkwong <150269506+lnkwong@users.noreply.github.com>
2023-11-27 14:16:48 -0600

    Add files via upload

--- module_chooser.r
+++ module_chooser.r
@@ -10,9 +10,9 @@ f  = sub('.txt','',f0)
 f1 = paste0(f,'.txt')
 f2 = paste0(f,'_Spear.txt')
 f3 = paste0(f,'_rank_Dist_Output.txt')
-f4 = paste0(f,'_h_',h0,'.txt')
-f5 = paste0(f,'_h_rank_',h0,'.png')
-f6 = paste0(f,'_h_heatmap','.png')
+f4 = paste0(f,'_h_0.95_',h0,'.txt')
+f5 = paste0(f,'_h_rank_0.95_',h0,'.png')
+f6 = paste0(f,'_h_heatmap_0.95','.png')
 
 table <- read.table(f1, header=TRUE, sep="\t",
                     row.names=1, stringsAsFactors=FALSE, comment.char="", check.names=FALSE)
@@ -32,6 +32,7 @@ sta0[sta0<0.95]<-0
 sta0d<-Dist(sta0,"correlation")
 write.table(round(as.matrix(sta0d),3),f3, row.names=TRUE, col.names=NA, quote=FALSE, sep="\t")
 sta0da<-hclust(sta0d,"average")
+sta0da$height <- round(sta0da$height, 6) 
 
 #perform cutree to file and for heatmap
 sta0dac<-cutree(sta0da,h = h0) 
@@ -39,9 +40,9 @@ write.table(sta0dac,f4,sep="\t") #output file 2
 sta0dad<-as.dendrogram(sta0da)
 
 #generate dendrogram to see node height
-png(file=f5) #output file 1
-plot(sta0da,hang=-1)
-dev.off()
+#png(file=f5) #output file 1
+#plot(sta0da,hang=-1)
+#dev.off()
 
 #generate heatmap
 png(file=f6) #output file 3
@@ -54,9 +55,9 @@ moduk <- function(f0, k0 = 5) {
   f1 = paste0(f,'.txt')
   f2 = paste0(f,'_Spear.txt')
   f3 = paste0(f,'_rank_Dist_Output.txt')
-  f4 = paste0(f,'_k_',k0,'.txt')
-  f5 = paste0(f,'_k_rank_',k0,'.png')
-  f6 = paste0(f,'_k_heatmap_','.png')
+  f4 = paste0(f,'_k_0.95_',k0,'.txt')
+  f5 = paste0(f,'_k_rank_0.95_',k0,'.png')
+  f6 = paste0(f,'_k_heatmap_0.95_','.png')
   
   table <- read.table(f1, header=TRUE, sep="\t",
                       row.names=1, stringsAsFactors=FALSE, comment.char="", check.names=FALSE)
@@ -76,6 +77,7 @@ moduk <- function(f0, k0 = 5) {
   sta0d<-Dist(sta0,"correlation")
   write.table(round(as.matrix(sta0d),3),f3, row.names=TRUE, col.names=NA, quote=FALSE, sep="\t")
   sta0da<-hclust(sta0d,"average")
+  sta0da$height <- round(sta0da$height, 6) 
   
   #perform cutree to file and for heatmap
   sta0dac<-cutree(sta0da,k = k0) 
@@ -98,6 +100,7 @@ modureh <- function(f0, h0 = 0.8) {
   sta0d <- as.dist(read.table(f3, header=TRUE, sep="\t",
                      row.names=1, stringsAsFactors=FALSE, comment.char="", check.names=FALSE))
   sta0da<-hclust(sta0d,"average")
+  sta0da$height <- round(sta0da$height, 6) 
   
   #perform cutree to file and for heatmap
   sta0dac<-cutree(sta0da,h = h0) 
@@ -115,6 +118,7 @@ modurek <- function(f0, k0 = 5) {
   sta0d <- as.dist(read.table(f3, header=TRUE, sep="\t",
                               row.names=1, stringsAsFactors=FALSE, comment.char="", check.names=FALSE))
   sta0da<-hclust(sta0d,"average")
+  sta0da$height <- round(sta0da$height, 6) 
   
   #perform cutree to file and for heatmap
   sta0dac<-cutree(sta0da,k = k0) 
